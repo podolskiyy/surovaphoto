5c4acf6987623cbba2db14dffee551c677e6169c
Miro <vladislav.gulyakin@gmail.com>
2022-11-09 17:47:24 -0500

    Fix: SendMailer

--- send.php
+++ send.php
@@ -1,24 +1,61 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $message = $_POST['message'];
+    
+    // $name = htmlspecialchars($name);
+    // $email = htmlspecialchars($email);
+    // $message = htmlspecialchars($message);
+    
+    // $name = urldecode($name);
+    // $email = urldecode($email);
+    // $message = urldecode($message);
+    
+    // $name = trim($name);
+    // $email = trim($email);
+    // $message = trim($message);
+    
+    // $to = "qumagipupu@rungel.net";
+    // $subject = "Test";
+    // $headers = "From:".$email;
+    
+    // mail($to, $subject, "TEXT", $headers);
+    // echo "";
 
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
-$message = htmlspecialchars($message);
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-$name = urldecode($name);
-$email = urldecode($email);
-$message = urldecode($message);
+    require 'path/to/PHPMailer/src/Exception.php';
+    require 'path/to/PHPMailer/src/PHPMailer.php';
+    require 'path/to/PHPMailer/src/SMTP.php';
 
-$name = trim($name);
-$email = trim($email);
-$message = trim($message);
+    require 'vendor/autoload.php';
 
-$to = "qumagipupu@rungel.net";
-$subject = "Test";
-$headers = "From:" . $email;
+    $mail = new PHPMailer(true);
 
-mail($to, $subject, $message, $headers);
-?>
+    try {
+
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Username = 'vladislav.gulyakin@gmail.com';
+        $mail->Password = viUiuyT3xHHXQX;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  
+        $mail->Port       = 465; 
+
+        $mail->setFrom($_POST["email"]);
+        $mail->addAddress('vladislav.gulyakin@gmail.com', 'Joe User'); 
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+
+        $mail-isHTML(true);
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+        $mail->send();
+
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
